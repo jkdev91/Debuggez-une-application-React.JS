edbe30aaf1c09890ea89015346cc864645bb8ab7
dev91junior <devjk91@outlook.com>
2024-03-30 12:33:34 +0100

    fix select catégory bug

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -13,12 +13,15 @@ const Select = ({
   label,
   type = "normal",
 }) => {
-  const [value, setValue] = useState();
+  // const [value, setValue] = useState();
+  const [value, setValue] = useState(null);
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
-    onChange();
+    // onChange();
+    onChange(newValue);
     setValue(newValue);
-    setCollapsed(newValue);
+    // setCollapsed(newValue);
+    setCollapsed(!collapsed);
   };
   return (
     <div className={`SelectContainer ${type}`} data-testid="select-testid">
